80f7674d39ac62b1e59419442e6c218be24c4b21
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-26 15:43:32 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    case array.length
+    when 1
+        return array[0]
+    when 2
+        return array[0] + " and " + array[1]
+    else
+        last_element = array.pop
+        return array.join(", ") + ", and " + last_element
+    end
 end
